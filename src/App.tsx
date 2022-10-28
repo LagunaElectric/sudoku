3b33898b8acf33edb8fa452091a6523b4fde7d58
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-10-28 03:40:05 -0400

    Removed the boilerplate UI and made the text lighter

--- src/App.tsx
+++ src/App.tsx
@@ -6,56 +6,21 @@ import './App.css';
 import { useAppSelector } from './app/hooks';
 import { selectGrid } from './features/sudoku/board/boardSlice';
 
+
+
 function App() {
   const grid = useAppSelector(selectGrid)
   return (
     <div className="App">
+      <h1>Sudoku</h1>
       <Board />
       <button style={ { marginTop: '25px' } } onClick={ () => console.log(grid) }>Log Grid</button>
-      <header className="App-header">
+      <header className="App-header" style={ { display: 'none' } }>
         <img src={ logo } className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
     </div>
   );
